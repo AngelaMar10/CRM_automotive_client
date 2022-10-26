302b9e730e4efba69503b173b8b60c0eaf5fb326
angelama <fotografahz@gmail.com>
2022-10-26 14:44:44 +1030

    updating repos

--- react-crm-app/src/App.js
+++ react-crm-app/src/App.js
@@ -1,53 +1,26 @@
 import React from 'react'
-import { useState } from 'react';
-import CustomerTable from './components/CustomerTable'
-// import ReactDOM from 'react-dom'
-// import { v4 as uuidv4 } from 'uuid';
-import './App.css';
-import AddCustomer from './components/AddCustomer';
+import Home from './components/Home'
+import Customers from './components/Customers'
 
-// As I work with local storage I am using a framework to allocate an id for each customer, this will help in my delete function.
-const App = () => {
+import { Routes, Route, Link } from 'react-router-dom'
 
-  const customersInfo = [
-    { id:1, name: 'Mayra Lav', rego: "SA456YU", phone: "0455223344", serviceDate: "02/09/2021" },
-    { id:2, name: 'Diego Smith', rego: "SA328TU", phone: "0455554899", serviceDate: "10/03/2022" }
-  ]
+function App() {
 
-  // Using useState with a previous information
-  const [customers, setCustomers] = useState(customersInfo)
-
-  // Function to add customers
-  const addCustomer = (customer) => {
-    customer.id = customers.length + 1
-    setCustomers([
-      //making a copy from customers (...)
-      ...customers, customer])
-
-  }
-  // Functio to delete customers
-  const deleteCustomer = id => {
-    setCustomers(customers.filter((customer) => customer.id !== id))
-  }
   return (
-    <div className="container">
-      <h1>DP Automotive services customers</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add customer</h2>
-          <AddCustomer 
-            addCustomer={addCustomer}/>
-        </div>
-        <div className="flex-large">
-          <h2>Customers Information</h2>
-          {/* To pass the customers information, I use props */}
-          <CustomerTable 
-            customers={customers} 
-            deleteCustomer={deleteCustomer}/>
-        </div>
-      </div>
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Customers">Customers</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home name='DP Automotive Service and Repais' />} />
+        <Route path='/Customer' element={<Home name='DP Automotive Service and Repais' />} />
+      </Routes>
     </div>
-  );
+  )
 }
+
+  
+
 
 export default App;
